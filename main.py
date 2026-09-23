902b2aedf983c2e2c06a40dd2e2a36c586320d8f
Thiago <tgverissimo2005@gmail.com>
2026-09-23 17:20:03 +0100

    Adicionei SQLite e gestão de jogadores

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
 from datetime import datetime
+import sqlite3
+bd_lanheses = sqlite3.connect("lanheses.db") #criamos a ligacao com a base de dados e guardamo-la na var bd_lanheses
+cursor = bd_lanheses.cursor() #criamos um cursor que ira mandar comandos SQL à BD
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS jogadores(
+id INTEGER PRIMARY KEY,
+nome TEXT,
+numero INTEGER,
+pos_prin TEXT,
+data_nascimento DATE,
+pe_dom TEXT
+)
+""") #criamos a tabela jogadores apenas se ela ja nao existir, com as colunas id, nome,...
+
+
 opcoes = """=== Lanheses DB ===
 
 1. Ver jogadores
 2. Adicionar jogador
-3. Sair
+3. Remover jogador
+4. Sair
 """
 print(opcoes)
 while True: #faz repetir até achar um break, fazemos isso pq assim nao precisamos de uma condicao no while
     try:
         opcao_escolhida = int(input("Escolha uma opção: ")) #input guarda um str e nao um int, entao transformamos manualmente
-        if opcao_escolhida not in [1,2,3]:
+        if opcao_escolhida not in [1,2,3,4]:
             print("Escolha uma opção válida")
         else: 
             break #sai do while quando o input é uma opcao válida (1,2,3)       
@@ -18,7 +34,14 @@ while True: #faz repetir até achar um break, fazemos isso pq assim nao precisam
 
 if opcao_escolhida == 1:
     # abre uma aba com a lista de jogadores ja adicionados na base de dados
-    print("Ver jogadores")
+    print("=== Ver jogadores ===")
+    cursor.execute("""
+    SELECT * FROM jogadores
+    """)
+    jogadores = cursor.fetchall() #pega em todos os resultados e coloca-os na var jogadores
+    #jogadores fica entao como uma lista de tuplos, cada um destes tuplos tem os atributos dos jogadores
+    for jogador in jogadores:
+        print(",".join( str(elem) for elem in jogador))
 
 elif opcao_escolhida == 2:
     # abre uma aba com um formulário a preencher para criar a ficha de um jogador na base de dados
@@ -52,7 +75,7 @@ elif opcao_escolhida == 2:
         try:
             posicoes_secundarias = [] #criamos a lista vazia primeiro
             entrada_valida = True #flag para saber se podemos sair do while ou nao
-            texto = input("Escolha asposições secundárias: ")
+            texto = input("Escolha as posições secundárias: ")
             if texto == "": # caso o user nao selecione nenhuma pos sec, fazemos break e saimos do while
                 break
             escolhas = texto.split(",")
@@ -76,6 +99,110 @@ elif opcao_escolhida == 2:
             break
         except ValueError:
             print("Insira uma data válida")
+
+    lista_nacionalidades = ["Português", "Brasileiro", "Angolano", "Francês", "Argentino", "Espanhol", "Cabo Verdiano"]
+    print("Nacionalidades:")
+    for num, nacionalidade in enumerate(lista_nacionalidades, start=1):
+        print(num, nacionalidade)
+    while True:
+        try:
+            nacionalidades_escolhidas= []
+            entrada_valida = True
+            texto = input("Escolha os números correspondentes às nacionalidades: ")
+            escolhas_nac = texto.split(",")
+
+            for n in escolhas_nac:
+                n = int(n)
+                if n not in range(1,len(lista_nacionalidades)+1):
+                    entrada_valida = False
+                    break #para sairmos do for
+                else:
+                    nacionalidades_escolhidas.append(lista_nacionalidades[n-1])
+
+        except ValueError:
+            print("Escolha nações válidas")
+            entrada_valida = False
+
+        if entrada_valida==True:
+            break
+    print("Pé dominante: ")
+    lista_pes = ["Direito", "Esquerdo", "Ambos"]
+    for (num,p) in enumerate(lista_pes, start=1):
+        print(num,p)
+    while True:
+        try:
+            pe_dominante = int(input("Escolha o número correspondente ao pé dominante: "))
+            if pe_dominante in range(1,len(lista_pes)+1):
+                pe_dominante = lista_pes[pe_dominante-1]
+                break
+            else:
+                print("Escolha um número válido")
+        except ValueError:
+            print("Escolha uma opção válida")
+
+    #=====================================================================
+    #INSERIR NA TABELA
+    #colocamos os atributos do jogador na base de dados
+    #colocamos ? em VALUES porque o SQLite nao interpreta diretamente as variaveis Python
+    #os valores das variaveis sao passados depois como parametros
+    cursor.execute("""
+    INSERT INTO jogadores (nome,numero,pos_prin, data_nascimento, pe_dom)
+    VALUES(?,?,?,?,?)
+    """, (nome, numero, pos, data_nascimento, pe_dominante))
+
+    bd_lanheses.commit() #serve para confirmar a alteração na BD
+    #=====================================================================
+    
+    print("===================================================")
+    print("=== Ficha do Jogador ===")
+    print("Nome: ", nome)
+    print("Número: ", numero)
+    print("Posição Principal: ", pos)
+    #print("Posições Secundárias: ")
+    #for i in posicoes_secundarias:
+    #    print(i)
+    print("Posições secundárias: ", ", ".join(posicoes_secundarias))
+
+    print("Data de Nascimento: ", data_nascimento)
+    #print("Nacionalidades: ")
+    #for j in nacionalidades_escolhidas:
+        #print(j)
+    print("Nacionalidades: ", ", ".join(nacionalidades_escolhidas))
+    print("Pé dominante: ", pe_dominante)
+    print("===================================================")
+    
+
+
+            
+
+        
 elif opcao_escolhida == 3:
+    print("=== Remover jogador ===")
+    cursor.execute("""
+    SELECT id, nome, numero FROM jogadores
+""")
+    jogadores = cursor.fetchall()
+    for jogador in jogadores:
+        print(",".join( str(elem) for elem in jogador))
+
+    #criamos uma lista com os ids de cada jogador para depois podermos fazer a validacao da escolha do user
+    #jogador[0] é o id do jogador
+    ids = [jogador[0] for jogador in jogadores]
+    while True:
+        try:
+            id_escolhido = int(input("Escolhe o ID do jogador a remover: "))
+            if id_escolhido not in ids:
+                print("Escolha um ID válido")
+            else:
+                cursor.execute("""
+                DELETE FROM jogadores
+                WHERE id = ?
+                """, (id_escolhido,))
+                bd_lanheses.commit()
+                break
+        except ValueError:
+            print("Escolha um ID válido")
+    
+elif opcao_escolhida == 4:
     print("Até logo!")
 
